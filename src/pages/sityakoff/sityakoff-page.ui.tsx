94d9863ddae44ff044cc1080b6272647a31ee7ac
pnovikov <spl.novikov@gmail.com>
2025-12-02 15:08:14 +0300

    slides page

--- src/pages/sityakoff/sityakoff-page.ui.tsx
+++ src/pages/sityakoff/sityakoff-page.ui.tsx
@@ -1,24 +1,12 @@
-import { Card, List, Typography } from 'antd';
+import { Card, List } from 'antd';
 import { Link } from 'react-router-dom';
 import { getVisiblePresentations } from '~shared/lib/presentations.config';
 
-const { Title, Paragraph } = Typography;
-
-/**
- * Page displaying all available presentations with links
- * Accessible at /sityakoff
- */
 export default function SityakoffPage() {
   const visiblePresentations = getVisiblePresentations();
 
   return (
     <Card>
-      <Title level={1}>Презентации Sityakoff</Title>
-      <Paragraph>
-        Здесь собраны все доступные презентации. Кликните на название, чтобы
-        открыть нужную презентацию.
-      </Paragraph>
-
       <List
         itemLayout="vertical"
         dataSource={visiblePresentations}
@@ -30,7 +18,6 @@ export default function SityakoffPage() {
                   {item.title} ({item.slug})
                 </Link>
               }
-              description={item.description}
             />
           </List.Item>
         )}
